010e1a9c864fb336d1880d44c3d174e9473e80e1
Fábio Nakatani <fabionaka@gmail.com>
2021-12-16 11:40:39 -0300

    finalizacao inicial do componente de album

--- src/app/components/album/album.component.ts
+++ src/app/components/album/album.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { SpotifyApiService } from 'src/app/shared/services/spotify-api.service';
+import { SpotifyAlbum } from 'src/app/shared/models/spotify-album'
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-album',
@@ -7,9 +11,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AlbumComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private spotifyApiService: SpotifyApiService,
+    private route: ActivatedRoute,
+    private router: Router,
+  ) { }
+  public resource: SpotifyAlbum | undefined
 
   ngOnInit(): void {
+    let albumId = this.route.snapshot.paramMap.get('albumId');
+    this.spotifyApiService.fetchAlbum(albumId).pipe(map(album => { return Object.assign(new SpotifyAlbum, album) })).subscribe((album: SpotifyAlbum) => {
+      this.resource = album;
+    })
   }
+  /**
+   * Navigate to a item type
+   *
+   * @param {*} item
+   * @returns {Promise<boolean>}
+   * @memberof AlbumComponent
+   */
+  open(item: any): Promise<boolean> {
+    return this.router.navigate([item.type, item.id])
+  }
+
 
 }
